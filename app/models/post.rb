57d3f42e141929d4bbd30272c7d431c016304e24
Itay Ariel <itayrel@gmail.com>
2018-06-03 09:29:46 +0300

    remove unused code

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,21 +1,3 @@
 class Post < ApplicationRecord
   belongs_to :feed
-
-  def save_from_job(post_params)
-    puts("in save from job!!! #{title}")
-    puts(post_params)
-    @post = Post.new(post_params)
-    if @post.save
-      puts('successfully saved!! ')
-    else
-      puts @post.errors
-    end
-  end
-
-
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def post_params
-    params.require(:post).permit(:title, :description, :link, :img_link, :html, :feed_id)
-  end
 end
